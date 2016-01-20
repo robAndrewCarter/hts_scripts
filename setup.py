5da87f6f2efd7ab3dc4c795ed0242bac7d422656
Rob Carter <r.andrew.carter@gmail.com>
2016-01-19 21:47:54 -0600

    First commit. Program cuurently samples bams, sorting and indexing each one

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='hts_scripts',
+      version='0.2.0',
+      description='Scripts for processing NGS data',
+      author='Rob Carter',
+      author_email='robert.carter@stjude.org',
+      #packages=['hlatyp'],
+      #package_data={'hlatyper': ['data/*']},
+      scripts=['scripts/sample_bam.py']
+     )
